5f852ab168a474bd93570470c8c98eb41b26fa5c
Andrew Kang <andrew716@gmail.com>
2018-04-13 03:14:19 -0700

    remove dummy test

--- test/test.js
+++ test/test.js
@@ -1,16 +1,6 @@
 const expect = require('chai').expect
 const server = require('../server')
 
-describe('1 == 1', function() {
-  it('true', function() {
-    expect(1).to.equal(1)
-  })
-
-  it('true', function() {
-    expect(2).to.equal(2)
-  })
-})
-
 describe('ping', () => {
   it('should return pong', () => {
     return server.inject({
